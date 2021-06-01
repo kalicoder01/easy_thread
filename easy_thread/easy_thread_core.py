857d64fa0e5cab0da32cb64ac5fef37b1a514e2f
vanproskur <vanproskur@gmail.com>
2021-06-01 09:34:24 +0300

    0.2

--- easy_thread/easy_thread_core.py
+++ easy_thread/easy_thread_core.py
@@ -1,5 +1,23 @@
 from .easy_thread_utils import *
 
-def easy_thread(func):
-	EasyThreadUtils(func).start()
 
+
+class EasyThread():
+	def __init__(self, func):
+		self.func = func
+		self.threads_name = []
+		self.stoppable = False
+
+	def run(self, amount = 1):
+		for i in range(amount):
+			easy_thread_util = EasyThreadUtils(self.func)	
+
+			easy_thread_util.start()
+			self.threads_name.append(easy_thread_util)	
+
+	def stop(self):
+		self.stoppable = True
+
+	def get_names(self) -> str:
+		return threads_name	
+		
